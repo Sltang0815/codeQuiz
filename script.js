6fbb14aaac3b478b4813fc78630119b52e872898
Sltang0815 <67080854+Sltang0815@users.noreply.github.com>
2020-09-09 22:00:31 -0700

    added to js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const questions = [
     {
         question: "Inside which HTML element do we put the JavaScript?",
         options: ["<script>", "<scripting>", "<javascript>", "<js>"],
-        a: "<script>",
+        answers: "<script>",
 
 
 
@@ -20,26 +20,26 @@ const questions = [
     {
         question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
         options: ["<script src='xxx.js'>", "<script name='xxx.js'>", "<script href='xxx.js'>"],
-        a: "<script src='xxx.js'>",
+        answers: "<script src='xxx.js'>",
 
 
     },
     {
         question: "How do you write 'Hello World' in an alert box?",
         options: ["alert('Hello World');", "alertBox('Hello World');", "msgBox('Hello World');", "msg('Hello World');"],
-        a: "alert('Hello World');",
+        answers: "alert('Hello World');",
 
     },
     {
         question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
         options: ["if (i != 5)", "if i =! 5 then", "if i <> 5", "if (i <> 5)"],
-        a: "if (i != 5)",
+        answers: "if (i != 5)",
 
     },
     {
         question: "How does a FOR loop start?",
         options: ["for (i = 0; i <= 5; i++)", "for (i = 0; i <= 5)", "for i = 1 to 5", "for (i <= 5; i++)"],
-        a: "for (i = 0; i <= 5; i++)",
+        answers: "for (i = 0; i <= 5; i++)",
 
     }
 
@@ -63,10 +63,12 @@ function gameOver() {
 
 }
 
-function startQuiz({
-    // pull objects options/questions
+/*function startQuiz() {
+    if (questions[answers])
+        let correctAns = answers;
+    // pull objects options
 
-})
+});*/
 
 startBtnel.addEventListener("click", function () {
     clock = setInterval(startTimer, 1000);
